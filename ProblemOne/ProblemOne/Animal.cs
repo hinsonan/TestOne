ce7ddcf4b94377b8ebdb7b2937c44cd0cef5928f
hinsonan <hinsonan@gmail.com>
2017-02-22 13:54:04 -0600

    solution to problem one

---
+++ ProblemOne/ProblemOne/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemOne
+{
+    abstract class Animal
+    {
+        public int Age { get; set; }
+        public double Weight { get; set; }
+
+        public Animal(int age, double weight)
+        {
+            Age = age;
+            Weight = weight;
+        }
+
+        abstract public void Move();
+
+    }
+}
---
+++ ProblemOne/ProblemOne/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemOne
+{
+    class Dog : Animal
+    {
+        public String Breed { get; set; }
+        public bool IsVaccinated { get; set; }
+
+        public Dog(int age, double weight,String breed, bool isItVaccinated) : base(age, weight)
+        {
+            Breed = breed;
+            IsVaccinated = isItVaccinated;
+            Age = age;
+            Weight = weight;
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("I'm a dog and I'm moving!");
+        }
+    }
+}

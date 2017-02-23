46d13fc80d83435011faa80f1061432fb5b4c523
hinsonan <hinsonan@gmail.com>
2017-02-22 21:19:56 -0600

    solution to problem two

---
+++ ProblemTwo/ProblemTwo/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemTwo
+{
+    class Cat : IAnimal
+    {
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public bool IsVaccinated { get; set; }
+
+        public Cat(int age, double weight, bool isItVaccinated)
+        {
+            Age = age;
+            Weight = weight;
+            IsVaccinated = isItVaccinated;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("I’m a cat and I’m moving!");
+        }
+    }
+}
